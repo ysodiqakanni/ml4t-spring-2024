eeae42c138e4f3a63b105f93be2e7e31aa3be8ba
Sodiq Yusuff <ysodiqakanni@gmail.com>
2024-03-09 21:16:46 -0600

    implement bollinger bands method

--- indicator_evaluation/indicators.py
+++ indicator_evaluation/indicators.py
@@ -10,6 +10,7 @@ from util import get_data
 import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
+import math
 
 def author():
     return 'syusuff3'
@@ -49,23 +50,49 @@ def get_sma(symbols, start_date, end_date, lookback=14):
             sma.ix[day, sym] /= lookback
     return df_prices, sma
 
-def plot_sma(prices, sma):
+def plot_sma(prices, sma, sma_long_term):
     # get the prices
     plt.figure()
     ax = prices.plot(color='r')
     sma.plot(color='g', ax=ax)
+    sma_long_term.plot(color='b', ax=ax)
 
     plt.grid(True)
     plt.title("SMA vs Adjusted closing price for JPM")
     plt.xlabel("Date")
     plt.ylabel("Price")
-    plt.legend(["Closed prices", "SMA"])
+    plt.legend(["Closed prices", "SMA", "SMA long term"])
     plt.show()
+
+def generate_bollinger_bands(symbols, sma, start_date, end_date, df_prices=None, lookback=14):
+    if df_prices is None:
+        df_prices_all = get_data(symbols, pd.date_range(start_date, end_date))
+        df_prices = df_prices_all[symbols]
+
+    bbp = df_prices.copy()
+    bbp[:,:] = 0
+    for day in range(df_prices.shape[0]):
+        for sym in symbols:
+            # calculate the standard deviation over the lookback period. std = sum(x - mean)^2
+            for prev_day in range(day-lookback+1, day+1):
+                bbp.ix[day, sym] += math.pow(df_prices.ix[prev_day, sym] - sma.ix[day, sym], 2)
+
+            # complete the standard deviation calculation
+            bbp.ix[day,sym] = math.sqrt(bbp.ix[day,sym]/(lookback-1))
+
+            # complete the calculation of BB% for this day and symbol
+            bottom_band = sma.ix[day,sym] - (2 * bbp.ix[day,sym])
+            top_band = sma.ix[day, sym] + (2 * bbp.ix[day, sym])
+
+            bbp.ix[day,sym] = (df_prices.ix[day, sym] - bottom_band) / (top_band - bottom_band)
+
 def run(symbols, start_date, end_date, lookback=14):
-    df_prices, df_sma = get_sma(symbols, start_date, end_date, lookback)
+    df_prices, df_sma = get_sma(symbols, start_date, end_date, 14)
+    df_prices, df_sma_long_term = get_sma(symbols, start_date, end_date, 50)
 
     # now plot sma over price
-    plot_sma(df_prices, df_sma)
+    plot_sma(df_prices, df_sma,df_sma_long_term)
+
 
 if __name__ == '__main__':
     print("running...")
