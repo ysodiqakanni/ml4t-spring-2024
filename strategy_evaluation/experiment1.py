78d26cfded55914ad365785587a3272238c2d0a9
Sodiq Yusuff <ysodiqakanni@gmail.com>
2024-04-19 18:32:33 -0500

    continue experiment 1

--- strategy_evaluation/experiment1.py
+++ strategy_evaluation/experiment1.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 import ManualStrategy as ms
 import StrategyLearner as sl
 import datetime as dt
@@ -5,11 +7,40 @@ from marketsimcode import compute_portvals
 def author():
     return 'syusuff3'
 
+def compute_and_plot(start_date, end_date):
+    impact, commission = 0.005, 9.95
+    symbol = "JPM"
+    start_val = 100000
+
+    manual_learner = ms.ManualStrategy(verbose=False, impact=impact, commission=commission)
+    manual_trades = manual_learner.testPolicy(symbol=symbol, sd=start_date, ed=end_date)
+    manual_portvals = compute_portvals(manual_trades, symbol, start_val, commission, impact)
+
+    strategy_learner = sl.StrategyLearner(verbose=False, impact=impact, commission=commission)
+    strategy_learner.add_evidence(symbol=symbol, sd=start_date, ed=end_date)
+    strategy_trades = strategy_learner.testPolicy(symbol=symbol, sd=start_date, ed=end_date)
+    strategy_portvals = compute_portvals(strategy_trades, symbol, start_val=start_val, commission=commission,
+                                         impact=impact)
+
+    benchmark_trades = pd.DataFrame(columns=[symbol], index=[manual_trades.index[0]])  # manual_trades.copy()
+    benchmark_trades.ix[manual_trades.index[0], symbol] = 1000
+    benchmark_portvals = compute_portvals(benchmark_trades, symbol, start_val=start_val, commission=commission, impact=impact)
+
+
+
 def run_experiment():
     # comparing manual and strategy learners
     # A. using in-sample JPM, get the trades df from manual and strategy
     # period is from January 1, 2008, to December 31, 2009.
     # Commission: $9.95, Impact: 0.005
+
+    # in-sample
+    compute_and_plot(dt.datetime(2008,1,1), dt.datetime(2009,12,31))
+    # out-sample
+    compute_and_plot(dt.datetime(2010,1,1), dt.datetime(2011,12,31))
+
+
+    return
     impact, commission = 0.005, 9.95
     symbol = "JPM"
     start_val = 100000
@@ -22,6 +53,10 @@ def run_experiment():
     strategy_learner.add_evidence(symbol=symbol, sd=start_date, ed=end_date)
     strategy_trades = strategy_learner.testPolicy(symbol=symbol, sd=start_date, ed=end_date)
     strategy_portvals = compute_portvals(strategy_trades, symbol, start_val=start_val, commission=commission, impact=impact)
+
+    benchmark_trades_in_sample = pd.DataFrame(columns=[symbol], index=[manual_trades.index[0]])  # manual_trades.copy()
+    benchmark_trades_in_sample.ix[manual_trades.index[0], symbol] = 1000
+    #benchmark_trades_in_sample.append({symbol:1000})
 
     # Now let's test out of sample
     os_start_date, os_end_date = dt.datetime(2010,1,1), dt.datetime(2011,12,31)
